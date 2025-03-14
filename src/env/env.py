5ad5852dc31d92876172f9c23e476dbcc660df06
KauanHK <kauankaestner06@gmail.com>
2025-03-14 17:57:24 -0300

    Fix: FlappyBird.step() retorna uma lista com o estado de todos os pássaros e cálculo da distância do pássaro para o cano corrigido

--- src/env/env.py
+++ src/env/env.py
@@ -37,7 +37,7 @@ class FlappyBird:
             self._screen = pg.display.set_mode(SCREEN_SIZE)
             self._clock = pg.time.Clock()
 
-    def step(self, actions: list[Literal[0, 1] | bool]) -> State:
+    def step(self, actions: list[Literal[0, 1] | bool]) -> list[State]:
         """Executa uma etapa no ambiente retorna o estado do jogo.
 
         :param actions: lista com as ações de cada pássaro.
@@ -52,30 +52,50 @@ class FlappyBird:
         next_pipe = self.pipes.get_next_pipe(Bird.X)
         scored = self.next_pipe != next_pipe
         self.next_pipe = next_pipe
+
         if scored:
             self.score += 1
             self._surface_score = self._create_surface_score()
 
+        states: list[State] = []
         for bird, action in zip(self.birds, actions):
+
             bird.update(bool(action), next_pipe)
-            if not bird.is_dead:
-                bird.score += scored
+            if bird.is_dead:
+                states.append(State(0, 0, True))
+                continue
+            bird.score += scored
+
+            # A distância do pássaro para o cano do topo é
+            # a distância entre o bottomleft do cano do topo e o topright do pássaro
+            # A distância do pássaro para o cano de baixo é
+            # a distância entre o topleft do cano de baixo e o bottomright do pássaro
+            # Usa-se pitágoras para fazer esse cálculo
+            # d^2 = dx^2 + dy^2
+            # SE O PÁSSARO ESTIVER ENTRE OS CANOS, A DISTÂNCIA É APENAS A DIFERENÇA DO EIXO Y
+
+            bird_right = Bird.X + Bird.WIDTH
+            bird_top = bird.y
+            bird_bottom = bird.y + Bird.HEIGHT
+
+            pipe_left = next_pipe.x
+            pipe_lower_top = next_pipe.y_lower
+            pipe_upper_bottom = next_pipe.y_upper + Pipe.HEIGHT
+
+            if bird_right >= next_pipe.x:
+                states.append(State(bird_top - pipe_upper_bottom, pipe_lower_top - bird_bottom, True))
+                continue
+
+            states.append(State(
+                math.sqrt((pipe_left - bird_top) ** 2 + (pipe_upper_bottom - bird_top) ** 2),
+                math.sqrt((pipe_left - bird_right) ** 2 + (pipe_lower_top - bird_bottom) ** 2),
+                False
+            ))
 
         birds_alive = list(filter(lambda b: not b.is_dead, self.birds))
-        if not birds_alive:
-            distance_upper = 0
-            distance_lower = 0
-            self.done = True
-        else:
-            bird = birds_alive[0]
-            right = Bird.X + Bird.WIDTH
-            middle_right = bird.y + Bird.HEIGHT//2
+        self.done = not birds_alive
 
-            square_distance_x = (next_pipe.x - right) ** 2
-            distance_upper = math.sqrt(square_distance_x + (next_pipe.y_upper-Pipe.HEIGHT - middle_right) ** 2)
-            distance_lower = math.sqrt(square_distance_x + (next_pipe.y_lower - middle_right) ** 2)
-
-        return State(distance_upper, distance_lower, self.done)
+        return states
 
     def render(self) -> None:
         """Renderiza o ambiente. Se o ambiente não
