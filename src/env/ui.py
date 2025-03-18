98961a35f22adff4d2c8ef79b0ad40444aa5f2b3
KauanHK <kauankaestner06@gmail.com>
2025-03-18 11:55:01 -0300

    Fix: Somente Bird's vivos são renderizados

--- src/env/ui.py
+++ src/env/ui.py
@@ -12,62 +12,58 @@ class FlappyBirdUI:
 
         pg.init()
 
-        self.score = 0
         self.background = Background()
         self._font = pg.font.SysFont('Arial', 48)
-        self._surface_score = self._create_surface_score()
+        self._surface_score = self._create_surface_score(0)
         self._surface_birds_alive = self._create_surface_birds_alive([])
         self._screen = pg.display.set_mode(SCREEN_SIZE)
         self._clock = pg.time.Clock()
 
     def reset(self) -> None:
 
-        self.background = Background()
-        self.score = 0
-        self._surface_score = self._create_surface_score()
+        self.background.reset()
+        self._surface_score = self._create_surface_score(0)
         self._surface_birds_alive = self._create_surface_birds_alive([])
 
-    def update(self, birds: list[Bird], scored: bool) -> None:
+    def update(self, birds_alive: int, score: int | None = None) -> None:
         """Executa uma etapa no ambiente retorna o estado do jogo.
 
-        :param actions: lista com as ações de cada pássaro.
+        :param score: Pontuação do jogo.
+        :param birds_alive: Número de pássaros vivos.
         """
 
         self.background.update()
-
-        if scored:
-            self.score += 1
-            self._surface_score = self._create_surface_score()
-
-        self._surface_birds_alive = self._create_surface_birds_alive(birds)
+        if score is not None:
+            self._surface_score = self._create_surface_score(score)
+        self._surface_birds_alive = self._create_surface_birds_alive(birds_alive)
 
     def render(self, birds: list[Bird], pipes: Pipes) -> None:
         """Renderiza o ambiente. Se o ambiente não
         estiver configurado para renderizar, lançará uma exceção."""
 
         self._screen.fill((0, 0, 0))
-
         self.background.render(self._screen)
 
         pipes.render(self._screen)
         for bird in birds:
-            if bird.is_alive:
-                bird.render(self._screen)
+            bird.render(self._screen)
 
         self._screen.blit(self._surface_score, (centralize_x(self._surface_score, SCREEN_CENTER_X)[0], 20))
         self._screen.blit(self._surface_birds_alive, (centralize_x(self._surface_birds_alive, SCREEN_CENTER_X)[0], 60))
 
-
         self._clock.tick(60)
         pg.display.flip()
 
-    def _create_surface_score(self) -> pg.Surface:
+    def close(self) -> None:
+        pg.quit()
+
+    def _create_surface_score(self, score: int) -> pg.Surface:
         """Atualiza o score."""
-        return self._font.render(str(self.score), True, (255, 255, 255))
+        return self._font.render(str(score), True, (255, 255, 255))
     
-    def _create_surface_birds_alive(self, birds: list[Bird]) -> pg.Surface:
+    def _create_surface_birds_alive(self, birds_alive: int) -> pg.Surface:
         """Atualiza o Surface do número de pássaros vivos."""
-        return self._font.render(str(len(tuple(filter(lambda b: b.is_alive, birds)))), True, (255, 255, 255))
+        return self._font.render(str(birds_alive), True, (255, 255, 255))
 
     def __repr__(self) -> str:
-        return f'FlappyBirdUI(score={self.score})'
+        return f'FlappyBirdUI()'
