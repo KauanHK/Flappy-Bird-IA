d911d9991cb3e2738472d21907819fdbf0d638e3
KauanHK <kauankaestner06@gmail.com>
2025-03-24 20:37:02 -0300

    Testes para Pipe e Pipes

--- tests/env/test_pipe.py
+++ tests/env/test_pipe.py
@@ -10,16 +10,18 @@ from collections import deque
 
 class TestPipe:
 
-    @pytest.fixture
-    def mock_surface(self, ):
+    @pytest.fixture(scope = 'class', autouse = True)
+    def mock_surface(self):
         """Fixture para criar um mock de pg.Surface."""
-        surface = Mock(spec=pg.Surface)
-        surface.get_width.return_value = Pipe.WIDTH
-        surface.get_height.return_value = Pipe.HEIGHT
-        return surface
 
+        mock_surface = Mock(spec = pg.Surface)
+        mock_surface.get_width.return_value = Pipe.WIDTH
+        mock_surface.get_height.return_value = Pipe.HEIGHT
 
-    def test_init(self, ):
+        with patch('src.env.pipe.load_img', return_value = mock_surface):
+            yield mock_surface
+
+    def test_init(self):
         """Testa a inicialização de um Pipe."""
 
         pipe = Pipe(x = 100, y_lower = 400)
@@ -31,24 +33,20 @@ class TestPipe:
     def test_get_image_upper(self, mock_surface):
         """Testa o método get_image_upper com lazy loading."""
 
-        with patch('src.env.pipe.load_img', return_value = mock_surface):
+        # Resetar _IMAGE_UPPER para None antes do teste
+        Pipe._IMAGE_UPPER = None
+        assert Pipe.get_image_upper() == mock_surface
+        assert Pipe._IMAGE_UPPER == mock_surface
 
-            # Resetar _IMAGE_UPPER para None antes do teste
-            Pipe._IMAGE_UPPER = None
-            assert Pipe.get_image_upper() == mock_surface
-            assert Pipe._IMAGE_UPPER == mock_surface
-
-            # Chama novamente para verificar se retorna a mesma imagem
-            assert Pipe.get_image_upper() == mock_surface
+        # Chama novamente para verificar se retorna a mesma imagem
+        assert Pipe.get_image_upper() == mock_surface
 
 
     def test_get_image_lower(self, mock_surface):
         """Testa o método get_image_lower com lazy loading."""
 
-        with (
-            patch('src.env.pipe.load_img', return_value = mock_surface),
-            patch('pygame.transform.flip', return_value = mock_surface)
-        ):
+        with patch('pygame.transform.flip', return_value = mock_surface):
+
             Pipe._IMAGE_LOWER = None
             assert Pipe.get_image_lower() == mock_surface
             assert Pipe._IMAGE_LOWER == mock_surface
@@ -58,22 +56,18 @@ class TestPipe:
     def test_get_mask_upper(self, mock_surface):
         """Testa o método get_mask_upper com lazy loading."""
 
-        with patch('src.env.pipe.load_img', return_value = mock_surface):
-
-            Pipe._MASK_UPPER = None
-            mask = Pipe.get_mask_upper()
-            assert isinstance(mask, MockMask)
-            assert Pipe._MASK_UPPER == mask
-            assert Pipe.get_mask_upper() == mask
+        Pipe._MASK_UPPER = None
+        mask = Pipe.get_mask_upper()
+        assert isinstance(mask, MockMask)
+        assert Pipe._MASK_UPPER == mask
+        assert Pipe.get_mask_upper() == mask
 
 
     def test_get_mask_lower(self, mock_surface):
         """Testa o método get_mask_lower com lazy loading."""
 
-        with (
-            patch('src.env.pipe.load_img', return_value=mock_surface),
-            patch('pygame.transform.flip', return_value=mock_surface)
-        ):
+        with patch('pygame.transform.flip', return_value=mock_surface):
+
             Pipe._MASK_LOWER = None
             mask = Pipe.get_mask_lower()
             assert isinstance(mask, MockMask)
@@ -140,7 +134,7 @@ class TestPipes:
         pipes = Pipes(x_start = SCREEN_WIDTH // 2, distance = SCREEN_WIDTH // 10)
 
         # Nesse caso, seis canos devem ter sido criados
-        assert len(pipes) == 6
+        assert len(pipes) == 3
         x = SCREEN_WIDTH // 2
         for pipe in pipes:
             assert pipe.x == x
@@ -151,27 +145,38 @@ class TestPipes:
 
         pipes = Pipes()
         initial_len = len(pipes.pipes)
+        lefts = [pipe.x for pipe in pipes.pipes]
+
         pipes.update()
-        assert len(pipes.pipes) == initial_len
-        for pipe in pipes.pipes:
-            assert pipe.x == pipe.x - 3  # Velocidade simulada em MockPipe
+        assert len(pipes) == initial_len
+        for pipe, x in zip(pipes, lefts):
+            assert pipe.x == x - 3
 
     def test_update_pop_and_append(self):
         """Testa o método update com remoção e adição de canos."""
+
         pipes = Pipes(x_start=400, distance=200)
-        pipes.pipes[0].x = -MockPipe.WIDTH - 1  # Força remoção
-        pipes.pipes[-1].x = self.SCREEN_WIDTH - MockPipe.WIDTH - pipes.distance - 1  # Força adição
-        initial_len = len(pipes.pipes)
+
+        initial_len = len(pipes)
+        first_pipe = pipes.pipes[0]
+        first_pipe.x = -Pipe.WIDTH * 2
         pipes.update()
-        assert len(pipes.pipes) == initial_len  # Pop e append mantêm o comprimento
-        assert pipes.pipes[0].x > -MockPipe.WIDTH  # Primeiro cano foi removido
-        assert pipes.pipes[-1].x == pipes.pipes[-2].x + MockPipe.WIDTH + 200
+        assert first_pipe not in pipes
+        assert len(pipes) == initial_len - 1
+
+        initial_len = len(pipes)
+        last_pipe = pipes[-1]
+        last_pipe.x = SCREEN_WIDTH - Pipe.WIDTH - pipes.distance - 1
+        pipes.update()
+        assert last_pipe in pipes
+        assert len(pipes) == initial_len + 1
 
     def test_render(self):
         """Testa o método render."""
+
         screen = Mock(spec=pg.Surface)
         pipes = Pipes(x_start=400, distance=200)
-        with patch.object(MockPipe, 'render') as mock_render:
+        with patch.object(Pipe, 'render') as mock_render:
             pipes.render(screen)
             assert mock_render.call_count == len(pipes.pipes)
             for pipe in pipes.pipes:
@@ -179,21 +184,24 @@ class TestPipes:
 
     def test_get_next_pipes(self):
         """Testa o método get_next_pipes."""
+
         pipes = Pipes(x_start=400, distance=200)
         pipe1, pipe2 = pipes.get_next_pipes(x=300)
         assert pipe1 == pipes.pipes[0]
         assert pipe2 == pipes.pipes[1]
+
         # Testa quando x está depois do último pipe
-        with pytest.raises(ValueError, match='Valor de x inválido'):
+        with pytest.raises(ValueError):
             pipes.get_next_pipes(x=1000)
+
         # Testa o último pipe
-        last_pipe, next_pipe = pipes.get_next_pipes(x=pipes.pipes[-1].x)
-        assert last_pipe == pipes.pipes[-1]
-        assert next_pipe == pipes.pipes[-1]
+        pipe1, pipe2 = pipes.get_next_pipes(x=pipes.pipes[-1].x)
+        assert pipe1 is pipe2
 
     def test_getitem(self):
         """Testa o método __getitem__."""
-        pipes = Pipes(x_start=400, distance=200)
+
+        pipes = Pipes(x_start = 400, distance = 200)
         assert pipes[0] == pipes.pipes[0]
         assert pipes[-1] == pipes.pipes[-1]
         with pytest.raises(IndexError):
@@ -201,18 +209,14 @@ class TestPipes:
 
     def test_iter(self):
         """Testa o método __iter__."""
-        pipes = Pipes(x_start=400, distance=200)
+        
+        pipes = Pipes(x_start = 400, distance = 200)
         iterated_pipes = list(iter(pipes))
         assert len(iterated_pipes) == len(pipes.pipes)
         assert all(p1 == p2 for p1, p2 in zip(iterated_pipes, pipes.pipes))
 
     def test_len(self):
         """Testa o método __len__."""
-        pipes = Pipes(x_start=400, distance=200)
-        assert len(pipes) == len(pipes.pipes)
 
-    def test_repr(self):
-        """Testa o método __repr__."""
-        pipes = Pipes(x_start=400, distance=200)
-        expected_repr = f'Pipes({pipes.pipes})'
-        assert repr(pipes) == expected_repr
+        pipes = Pipes()
+        assert len(pipes) == len(pipes.pipes)
